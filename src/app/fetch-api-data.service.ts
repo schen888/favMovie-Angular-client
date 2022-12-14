794a0b412444f6bb96f216eb795dd40a53630126
schen888 <shuang.chen888@hotmail.com>
2022-12-14 20:21:07 +0100

    Set up the fetch-api-data service

---
+++ src/app/fetch-api-data.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+const apiUrl = 'https://favmovie-schen.herokuapp.com/';
+@Injectable({
+  providedIn: 'root'
+})
+
+export class  FetchApiDataService {
+  // Inject the HttpClient module to the constructor params
+ // This will provide HttpClient to the entire class, making it available via this.http
+  constructor(private http: HttpClient) {}
+
+ // Making the api call for the user registration endpoint (posts it to the API endpoint and returns the API's response)
+  public userRegistration(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http
+      .post(apiUrl + 'users', userDetails)
+      .pipe(catchError(this.handleError));
+  }
+
+  //User login call
+  public userLogin(userDetails: any): Observable<any> {
+    return this.http
+      .post(apiUrl + 'login', userDetails)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse): any {
+      if (error.error instanceof ErrorEvent) {
+      console.error('Some error occurred:', error.error.message);
+      } else {
+      console.error(
+          `Error Status code ${error.status}, ` +
+          `Error body is: ${error.error}`);
+      }
+      return throwError(
+    'Something bad happened; please try again later.');
+  }
+
+  getAllMovies(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http
+      .get(apiUrl + 'movies', {
+        headers: new HttpHeaders(
+        {
+          Authorization: 'Bearer ' + token,
+        })
+      })
+      .pipe(map(this.extractResponseData),catchError(this.handleError));
+  }
+// Non-typed response extraction
+  private extractResponseData(res: any): any {
+    const body = res;
+    return body || { };
+  }
+}
+
+/* 
+export class FetchApiDataService {
+
+  constructor() { }
+}
+ */
