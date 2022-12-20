18d437c4aad41b13c660fae60e85ff96b6075e7a
schen888 <shuang.chen888@hotmail.com>
2022-12-20 14:27:18 +0100

    Add router to movies

---
+++ src/app/genre/genre.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-genre',
+  templateUrl: './genre.component.html',
+  styleUrls: ['./genre.component.scss']
+})
+export class GenreComponent {
+  @Input() genre = {Name: '', Description: ''};
+
+  
+}
